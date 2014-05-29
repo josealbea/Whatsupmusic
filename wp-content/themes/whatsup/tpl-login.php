6c374e2059863eed4bb215df6c5078a25e69e01b
yohann-teisseire <yohan.teisseire@gmail.com>
2014-05-29 14:58:34 +0200

    style btn connect

--- wp-content/themes/whatsup/tpl-login.php
+++ wp-content/themes/whatsup/tpl-login.php
@@ -12,6 +12,14 @@ get_header();
 <div class="single">
     <div class="post background">
         <h1><?php the_title(); ?></h1>
+        <div class="btn-connect">
+            <a href="http://localhost/WhatsUpMusic/wp-login.php?loginFacebook=1&redirect=http://localhost/WhatsUpMusic" onclick="window.location = 'http://localhost/WhatsUpMusic/wp-login.php?loginFacebook=1&redirect='+window.location.href; return false;">
+                <div class="new-fb-btn new-fb-1 new-fb-default-anim"><div class="new-fb-1-1"><div class="new-fb-1-1-1">FACEBOOK</div></div></div>
+            </a>
+            <a href="http://localhost/WhatsUpMusic/wp-login.php?loginTwitter=1&redirect=http://localhost/WhatsUpMusic" onclick="window.location = 'http://localhost/WhatsUpMusic/wp-login.php?loginTwitter=1&redirect='+window.location.href; return false;">
+                <div class="new-twitter-btn new-twitter-1 new-twitter-default-anim"><div class="new-twitter-1-1"><div class="new-twitter-1-1-1">TWITTER</div></div></div>
+            </a>
+        </div>
         <?php 
             if ( have_posts() ) {
                 while ( have_posts() ) {
@@ -22,12 +30,4 @@ get_header();
         ?>
     </div>
 </div>
-<a href="http://localhost/WhatsUpMusic/wp-login.php?loginFacebook=1&redirect=http://localhost/WhatsUpMusic" onclick="window.location = 'http://localhost/WhatsUpMusic/wp-login.php?loginFacebook=1&redirect='+window.location.href; return false;">
-    <div class="new-fb-btn new-fb-1 new-fb-default-anim"><div class="new-fb-1-1"><div class="new-fb-1-1-1">CONNECT WITH</div></div></div>
-</a>
-
-<a href="http://localhost/WhatsUpMusic/wp-login.php?loginTwitter=1&redirect=http://localhost/WhatsUpMusic" onclick="window.location = 'http://localhost/WhatsUpMusic/wp-login.php?loginTwitter=1&redirect='+window.location.href; return false;">
-    <div class="new-twitter-btn new-twitter-1 new-twitter-default-anim"><div class="new-twitter-1-1"><div class="new-twitter-1-1-1">CONNECT WITH</div></div></div>
-</a>
-
 <?php get_footer(); ?>
